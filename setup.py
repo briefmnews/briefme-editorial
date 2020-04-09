81f4e66ade72473f1c7d9ad2d278177a0519e164
Thomas C <thomas.carbillet@gmail.com>
2020-04-09 17:34:47 +0200

    feat(): version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="briefme-editorial",
-    version="0.0.1",
+    version="0.0.2",
     description="Asbtract editorial base for Brief.me projects",
     url="https://github.com/briefmnews/briefme-editorial",
     author="Brief.me",
